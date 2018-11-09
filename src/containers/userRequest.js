dc05ed53bdd67e826019ab57416e400a8a5f518e
Alexey <32864666+EvdokimovAlexey@users.noreply.github.com>
2018-11-09 21:53:18 +0300

    master commit

--- src/containers/userRequest.js
+++ src/containers/userRequest.js
@@ -12,6 +12,32 @@ import Icon24Camera from '@vkontakte/icons/dist/24/camera';
 
 const osname = platform();
 
+const thematics = [
+    {id: 3201, name: "Аренда автомобилей"},
+    {id: 3273, name: "Автотовары"},
+    {id: 3205, name: "Автосалон"},
+    {id: 3282, name: "Автосервис"},
+    {id: 3283, name: "Услуги для автовладельцев"},
+    {id: 3284, name: "Велосипеды"},
+    {id: 3285, name: "Мотоциклы и другая мототехника"},
+    {id: 3286, name: "Водный транспорт"},
+    {id: 3287, name: "Автопроизводитель"},
+    {id: 3288, name: "Автомойка"},
+    {id: 3117, name: "Автошкола"},
+    {id: 3118, name: "Детский сад"},
+    {id: 3119, name: "Гимназия"},
+    {id: 3120, name: "Колледж"},
+    {id: 3121, name: "Лицей"},
+    {id: 3122, name: "Техникум"},
+    {id: 3123, name: "Университет"},
+    {id: 3124, name: "Школа"},
+    {id: 3125, name: "Институт"},
+    {id: 3126, name: "Обучающие курсы"},
+    {id: 3276, name: "Дополнительное образование"},
+    {id: 3275, name: "Тренинг, семинар"},
+    {id: 3127, name: "Танцевальная школа"}
+  ];
+
 const UserRequest = props => (
   <Panel id={props.id}>
     <PanelHeader left={<HeaderButton onClick={props.go} data-to="home">
@@ -20,10 +46,7 @@ const UserRequest = props => (
     <FormLayout>
       <Textarea top="Описание" placeholder="Оставтье описание заявки" />
       <Select top="Номер зала" placeholder="Выберите номер зала">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">2</option>
-        <option value="4">2</option>
+        {thematics.map(thematic => <option value={thematic.id}>{thematic.name}</option>)}
       </Select>
       <File top="Загрузите фото" before={<Icon24Camera />} size="l">
         Открыть галерею
