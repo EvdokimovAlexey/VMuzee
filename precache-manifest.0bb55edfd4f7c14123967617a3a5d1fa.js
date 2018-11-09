5b4fcbd6889f8156e8aa31e1ccdef907c4276abe
Alexey <32864666+EvdokimovAlexey@users.noreply.github.com>
2018-11-09 19:12:36 +0300

    Updates

---
+++ precache-manifest.0bb55edfd4f7c14123967617a3a5d1fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8842954b78f8fc59723",
+    "url": "/VMuzee/static/css/main.504e0427.chunk.css"
+  },
+  {
+    "revision": "c8842954b78f8fc59723",
+    "url": "/VMuzee/static/js/main.c8842954.chunk.js"
+  },
+  {
+    "revision": "7d2cd58fc29e1d7710b2",
+    "url": "/VMuzee/static/css/1.31b2523e.chunk.css"
+  },
+  {
+    "revision": "7d2cd58fc29e1d7710b2",
+    "url": "/VMuzee/static/js/1.7d2cd58f.chunk.js"
+  },
+  {
+    "revision": "d3294f25197e95c2ed7d",
+    "url": "/VMuzee/static/js/runtime~main.d3294f25.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VMuzee/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "78f8d742056947b59e600416254b605c",
+    "url": "/VMuzee/index.html"
+  }
+];
